b2c946c8bc23bf1055ebe376899b063753ccfc81
k8150 <kojihaigo@gmail.com>
2021-10-15 23:39:20 +0900

    Initial Commit

---
+++ TCABasicUsage/TCABasicUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCABasicUsageApp.swift
+//  TCABasicUsage
+//
+//  Created by haigo koji on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct TCABasicUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
